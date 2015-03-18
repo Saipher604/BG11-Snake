acc64c6482e6a2e84c97bc471cf586b7eb0b44d1
Saipher604 <ljsaipher013@googlemail.com>
2015-03-19 00:27:57 +0100

    Revert "big Update"
    This reverts commit 08d2e03bb706a6269c801494398cff80359f1802.

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -8,8 +8,6 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-#define ARENA_WIDTH 78
-#define ARENA_HEIGHT 23
 
 //Defines gotoxy() for ANSI C compilers.
 void gotoxy(short x, short y) {
@@ -17,59 +15,72 @@ COORD pos = {x, y};
 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
+
 int main(int argc, char** argv) {
 	
 	
 	// food variables
 	int  xf, yf, food = 0,  xff, yff, fakefood = 0;
 	
-	// Snake variables movement direction, Position, length, 
-	int  dir = 4, x[ARENA_WIDTH * ARENA_HEIGHT], y[ARENA_WIDTH * ARENA_HEIGHT],  horP = 40, verP = 12, n = 16;
+	// Snake variables movement direction, Position, length
+	int  dir = 4, x[10000], y[10000],  horP = 40, verP = 12, n = 16;
 	
-	// various variables for simple counting, points, difficulty and for temporaily caching snake lenght (n -> nsave)
-	int points, speed = 0, fcounter = 0, d, i, z = 0, chance, difficulty, b = 0, loop = 1, key, nsave;
 	
-	int Debug = 0, edebug = 0;
-	FILE *debug;
+	int points, i, z = 0, chance, color = 0, difficulty, b = 0, loop = 1, key, phan = 0, check, nsave;
+	
 	
 	// difficulty options
 	const int EASY = 100, MEDIUM = 80, HARD = 70;
-	
-	// constants for Keys
-	const int UP = 4, DOWN = 3, LEFT = 2, RIGHT = 1, W = 97,A = 119, S = 100, D = 115, ESC = 27, X = 120;
+	// constants for Keys for better readability
+	const int UP = 4, DOWN = 3, LEFT = 2, RIGHT = 1, W = 97,A = 119, S = 100, D = 115, ESC = 27;
 	
 	const int NO = 0, YES = 1;
 	
-	const int encryptionlevel = -133;
 	
 	char Snake = 219, blank = 255, ae /*ä*/ = 132, ue /*ü*/ = 129, oe /*ö*/ = 148;
 	
-	// character arrays for highscore names
+	char test = 240;
+	
 	char name[15] = {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255};
 	
 	// ASCII-code for Arena lines
 	char horz = 196, verz = 179, ol = 218, orr = 191, ul = 192, ur = 217;
 	
-	// overwrite position arrays with 0 !note! x[0] and y[0] are reserved for snake head!! checking for intersections must be done with 'n + 1'
-	for(i = 0; i < (ARENA_WIDTH * ARENA_HEIGHT); i++)
-	{
-		x[i] = 0;
-		y[i] = 0;
-	}
-	
-	// randomization for food spawner
 	srand(getpid());
 	
-	gotoxy(38, 12);
-	std:: cout << "Snake";
+	gotoxy(34, 12);
+	std:: cout << "MauProductions";
+	
+	for(i = 0; i <= 21; i++)
+	{
+		switch(color)
+		{
+			case 0:system("COLOR 0"); ;break;
+			case 1:system("COLOR 1"); ;break;
+			case 2:system("COLOR 2"); ;break;
+			case 3:system("COLOR 3"); ;break;
+			case 4:system("COLOR 4"); ;break;
+			case 5:system("COLOR 5"); ;break;
+			case 6:system("COLOR 6"); ;break;
+			case 7:system("COLOR 7"); ;break;
+			case 8:system("COLOR 8"); ;break;
+			case 9:system("COLOR 9"); ;break;
+			default:system("COLOR F");;
+		}
+		
+	 	Sleep(70);
+		if(color < 10 && i <10)
+			color++;
+		
+		else
+			color--;
+	}
 	
 	Sleep(600);
 	
 	while(loop == YES)
 	{
 		system("cls");
-		
-		// print main menu
 		gotoxy(3, 19);
 		std:: cout << "Steuerung: W - hoch";
 		gotoxy(14, 20);
@@ -86,88 +97,47 @@ int main(int argc, char** argv) {
 		gotoxy(23, 13);
 		std:: cout << "[4] Highscores";
 		
-		
-		/* draw debug menu text based on choosen option */
- 		if(Debug == 0)
-		{
-			gotoxy(23, 14);
-			std:: cout << "[0] Debug [AUS]";
-		}
-		else
-		{
-			gotoxy(23, 14);
-			std:: cout << "[0] Debug [AN]";
-			
-			if(edebug == 0)
-			{
-				gotoxy(23, 15);
-				std:: cout << "[9] exstensive Debug [AUS]";
-			}
-			
-			else if(edebug == 1)
-			{
-				gotoxy(23, 15);
-				std:: cout << "[9] exstensive Debug [AN]";
-			}
-			
-		}
-		/* - */
-		
 		gotoxy(23, 12);
 		std:: cout << "Schwierigkeitsgrad ausw" << ae << "hlen [1|2|3] ";
 		
-		
-		// read key presses for menu navigation
-		scanf("%d", &key);
+		scanf("%d", &z);
 		fflush(stdin);
 		
-		if(key == 1)
-			difficulty = EASY;
-			
-		if(key == 2)
-			difficulty = MEDIUM;
-			
-		if(key == 3)
-			difficulty = HARD;
-			
- 		if(key == 1 || key == 2 || key == 3)
+		if(z == 1)
+		difficulty = EASY;
+		if(z == 2)
+		difficulty = MEDIUM;
+		if(z == 3)
+		difficulty = HARD;
+ 		if(z == 1 || z == 2 || z == 3)
 	 	 	loop = NO;
-	 	 	
-	 	// Read and print Highscore file
-	 	if(key == 4)
+	 	if(z == 4)
 	 	{
 	 		system("cls");
 	 		gotoxy(0, 0);
 	 		
 	 		FILE *file;
-			
-			// open file for reading
+				
 			file = fopen("Highscores.txt", "r");
-			
-			// print decrypted contents of highscore.txt
+				
 			if (file)
 			{
 				z = 0;
-				d = 2;
-				std:: cout << "1	-	";
-	 			while ((i = fgetc(file) + encryptionlevel) - encryptionlevel != EOF)
+		 		while ((i = fgetc(file) + 10) - 10 != EOF)
 		 		{
         			putchar(i);
         			Sleep(10);
         			z++;
         			if(z == 22)
         			{
-        				std:: cout << "\n\n" << d << "	-	";
-        				d++;
-        				z = 0;
+        				std:: cout << "\n\n";
 					}
         		}
-        		// close file
     			fclose(file);
 			}
 			std:: cout << "\n\nzur" << ue << "ck mit beliebiger Taste | [d] Highscores l" << oe << "schen\n";
 			
-			_cscanf("%c", &key);	// wait for button press, 'd' deletes the highscore file
+			_cscanf("%c", &key);
 			fflush(stdin);
 			if(key == 100)
 			{
@@ -177,61 +147,17 @@ int main(int argc, char** argv) {
     				puts( "\n\nHighscores erfolgreich geloescht" );
 				getch();
 			}
+			z = 0;
  		}
- 		/* end read of highscore */
- 		
- 		/* debug mode - variables will be saved per frame in debug.txt */
-		// check if debug key has been pressed
- 		if(key == 0)
- 		{
- 			if(Debug == 0)
- 			{
- 				// remove potential old debug file
- 				remove( "Debug.txt" );
- 				Debug = 1;
- 			}
- 			else if(Debug == 1)
- 			{
- 				Debug = 0;
- 				edebug = 0;
- 			}
-		}
-		
-		// activation of extensive Debug mode, arrays will be fully saved per frame
-		if(key == 9)
-		{
-				if(edebug == 0)
-			{
-				edebug = 1;
-			}
-			else if(edebug == 1)
-			{
-				edebug = 0;
-			}
-		}
-		
-		/* end debug settings */
-		
 	}
 	
-	// reset loop variable
 	loop = YES;
 	system("cls");
-	
-	
-	if(Debug == 1)
-	{
-		// create debug file
-		debug = fopen("Debug.txt", "w");
-	}
-	
 	
 	/* SNAKE - main application */
 	
 	while(loop == YES)
 	{
-		
-		// draw points
 		gotoxy(0,0);
 		std:: cout << ol << "Punkte:" << points << horz;
 		
@@ -246,19 +172,19 @@ int main(int argc, char** argv) {
 			
 			nsave = n;
 			i = 0;
-			while(i - 1 <= nsave + 1)
+			while(i - 1 <= nsave)
 			{
 				i++;
 				
 				// check for identical position of food with Snake
-				if(xf == x[n + 1] && yf == y[n + 1])
+				if(xf == x[phan + n] && yf == y[phan + n])
 				{
 					xf = rand()%78 + 1;
 					yf = rand()%23 + 1;
 					i = 0;
 					n = nsave;
-				}// if the position matches the snake a new one will be generated
-				
+				}
+									// if the position matches the snake a new one will be generated
 				n--;
 				
 			}
@@ -269,21 +195,19 @@ int main(int argc, char** argv) {
 			
 			chance = rand()%10;
 			
-			if(chance == 0 && difficulty <= MEDIUM && fakefood == NO)
+			if(chance == 0 && difficulty <= 80 && fakefood == NO)
 			{
 				
-				// generation of fakefood position
 				xff = rand()%78 + 1;
 				yff = rand()%23 + 1;
 				
 				nsave = n;
 				i = 0;
-				while(i - 1 <= nsave + 1)
+				while(i - 1 <= nsave)
 				{
 					i++;
 					
-					// check for intersection with snake or normal food
-					if(xff == x[n + 1] && yff == y[n + 1] || xff == xf && yff == yf)
+					if(xff == x[phan + n] && yff == y[phan + n] || xff == xf && yff == yf)
 					{
 						xff = rand()%78 + 1;
 						yff = rand()%23 + 1;
@@ -322,55 +246,37 @@ int main(int argc, char** argv) {
 			system("COLOR 8F");
 			gotoxy(0, 0);
 			std:: cout << ol <<"!pausiert! - Esc zum beenden  Punkte:" << points;
-			
  				_cscanf("%c", &key);
  				fflush(stdin);
- 				
- 				// esc quits the game
 				if(key == ESC)
 				{
 					gotoxy(35, 12);
 					std:: cout << "Name: ";
-					
-					// generation of highscore file
+				
 					FILE *file;
 				
 					file = fopen("Highscores.txt", "a");
-					
-					// enter name
+				
 					fscanf(stdin, "%s", &name);
-					
-					// encryption of the name
+				
 					for(i = 0; i < 15; i++)
-			  			name[i] = name[i] - encryptionlevel;
+			  			name[i] = name[i] - 10;
 					
-					// declare digits for points
 					char c[7] = {255,255,255,255,255,255,255};
 					
 					sprintf(c, "%d", points);
 					
-					// encryption of the digits
 					for(i = 0; i < 7; i++) 
 					{
-				  		c[i] = c[i] - encryptionlevel;
+				  		c[i] = c[i]-10;
 					}
 					
-					// final writing of the .txt file
 					fprintf(file, "%s%s", name, c);
 				
 					fclose(file);
 					
-					/* end highscore file */
-					
-					// if debug is activated, close debug file
-					if(Debug == 1)
-					fclose(debug);
-					
-					// quit
 					return EXIT_SUCCESS;
 				}
-				
-				// resume the game
 				else
 				{
   				 	system("COLOR 0F");
@@ -392,25 +298,13 @@ int main(int argc, char** argv) {
 		if(key == S && dir != UP)
 			dir = DOWN;
 			
-		if(key == W  && dir != DOWN)
+		if(key == 97  && dir != DOWN)
 	 		dir = UP;
-	 		
-		if(key == X && speed == 0)	// X unlocks the speed limit, game will run as fast as the pc can process it
-		{
-  			speed = 1;
-  			key = 0;
-  		}
-		else if(key == X && speed == 1)
-	 	{
-			speed = 0;
-			key = 0;
-		}
 	 		
 	 		
 		switch (dir)
 		{
-			// horizontal 'horP' & vertical 'verP' position calculation of movement
-			case RIGHT:verP = verP + 1; if(verP == 24)verP = 1 ;;break;
+			case RIGHT:verP = verP + 1; if(verP == 24)verP = 1 ;;break;			// horizontal 'horP' & vertical 'verP' position calculation
 			case LEFT :verP = verP - 1; if(verP == 0 )verP = 23;;break;
 			case DOWN :horP = horP + 1; if(horP == 79)horP = 1 ;;break;
 			case UP	  :horP = horP - 1; if(horP == 0 )horP = 78;;break;
@@ -418,7 +312,7 @@ int main(int argc, char** argv) {
 		
 		/* food */
 		
-		// check if snake head intersects with food
+		// check if snake head has the same position as food
 		if(xf == horP && yf ==verP)
 		{
 			
@@ -430,13 +324,11 @@ int main(int argc, char** argv) {
 			// snake growth based on difficulty
 			if(difficulty != 60)
 				n++;
-			
-			// difficulty 3: double snake growth
+				
 			else
 				n = n + 2;
 			
-			// set food to eaten
-			food = NO;
+			food = NO;	// set food to eaten
 			
 			// points for eating
 			points = points + 20 + n;
@@ -472,10 +364,10 @@ int main(int argc, char** argv) {
 		/* check for snake head intersection with body */
 		
 		nsave = n;
-		for(i = 0; i < nsave + 1; i++)
+		for(i = 0; i < nsave; i++)
 		{
 			n--;
-			if(horP == x[n + 1] && verP == y[n + 1])		// intersection results in game over
+			if(horP == x[phan + n] && verP == y[phan + n])				// intersection results in game over
 			{
 				system("COLOR 4F");
 				gotoxy(0, 0);
@@ -491,8 +383,6 @@ int main(int argc, char** argv) {
 				gotoxy(34, 15);
 				std:: cout << "Punkte: " << points;
 				Sleep(600);
-				
-				// entry of name and highscore file generation
 				gotoxy(35, 17);
 				std:: cout << "Name: ";
 				
@@ -503,24 +393,20 @@ int main(int argc, char** argv) {
 				fscanf(stdin, "%s", &name);
 				
 				for(i = 0; i < 15; i++)
-			  		name[i] = name[i] - encryptionlevel;
+			  		name[i] = name[i] - 10;
 					
 				char c[7] = {255,255,255,255,255,255,255};
 				sprintf(c, "%d", points);
 				
 				for(i = 0; i < 7; i++) 
 				{
-			 		c[i] = c[i]-encryptionlevel;
+			 		c[i] = c[i]-10;
 				}
 				
 				fprintf(file, "%s%s", name, c);
 			
 				fclose(file);
 				
-				if(Debug == 1)
-				fclose(debug);
-				
-				// quit
 				return EXIT_SUCCESS;
 			}
 		}
@@ -529,26 +415,21 @@ int main(int argc, char** argv) {
 		/* - */
 		
 		// save current head position to array
-		x[0] = horP;
-		y[0] = verP;
-		
-		// move Snake position in array
-		for(i = 0; i <= n; i++)
-		{
-			x[n - i + 1] = x[n - i];
-			y[n - i + 1] = y[n - i];
-		}
+		x[phan + n] = horP;
+		y[phan + n] = verP;
 		
 		// removing snakes tail for movement
-		gotoxy(x[n + 1], y[n + 1]);
+		gotoxy(x[phan], y[phan]);
 		std:: cout << blank;
+		
+		phan = phan + 1;
 		
 		// draw next position of snake head
 		gotoxy(horP, verP);
 		std:: cout << Snake;
 		
-		// point limit for balancing
-		if(points < 500*(n - 15)/2 && difficulty != 100)
+		
+		if(points < 500*(n - 15)/2 && difficulty != 100)			// point limit for balancing
 		points++;
 		else if(points < 250*(n - 15)/2)
 		{
@@ -557,7 +438,7 @@ int main(int argc, char** argv) {
 		
 		/* drawing of map walls */
 		
-		for(i = b; i < 78; i++)
+		for(i = b; i < 80; i++)
 		{
 			if(i == 0)
 			{
@@ -573,7 +454,7 @@ int main(int argc, char** argv) {
 			}
 			gotoxy(i +1, 24);
 			std:: cout << horz;
-			if(i == 77)
+			if(i == 79)
 			{
 				gotoxy(79, 24);
 				std:: cout << ur;
@@ -593,60 +474,12 @@ int main(int argc, char** argv) {
 				std:: cout << orr;
 			}
 		}
-		b = 79;
+		b = 81;
 		
 		/* - */
 		
-		// updating of debug file
-		if(Debug == 1)
-		{
-			fprintf(debug, "\n\nFrame[%d]----------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\n", fcounter);
-			
-			// save various varables per frame
-			fprintf(debug, "direction: %d  ||  snake length: %d\n\n[Xposition: %d|Yposition: %d]\n\nfood: %d | foodX: %d/foodY: %d\n\nfakefood: %d | ffoodX: %d/ffoodY: %d\n\npressed Key: %c|%d\n\n", dir, n, horP, verP, food, xf, yf, fakefood, xff, yff, key, key);
-			
-			// extensive debug save snake position arrays
-			if(edebug == 1)
-			{
-				for(i = 0; i < ARENA_WIDTH * ARENA_HEIGHT; i++)
-				{
-					fprintf(debug, "[x%d]:	%d	-|-	[y%d]:	%d\n", i, x[i], i, y[i]);
-				}
-			}
-			 	key = 0;	// reset pressed key to avoid documentation of unnecessary key presses
-			 	
-			// increase frame counter
-			fcounter++;
-		}
-		
-		// increase frame counter
-		fcounter++;
-		
-		if(speed == 0)						// boost button may disable game cycle delay
-		usleep( difficulty * 1000);			// Wait for next game cycle, difficulty affects game speed
+		usleep(difficulty*1000);			// Wait for next game cycle, difficulty affects game speed
 	}
 	
-	/* end game loop */
-	
-	// potential breakout of the game loop results in error message and creation of debug file
-	if(Debug == 0)
-		{
-			debug = fopen("Debug.txt", "w");
-			fprintf(debug, "\n\n!Game loop fail!----------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\n");
-			
-			fprintf(debug, "direction: %d  ||  snake length: %d\n\n[Xposition: %d|Yposition: %d]\n\nfood: %d | foodX: %d/foodY: %d\n\nfakefood: %d | ffoodX: %d/ffoodY: %d\n\npressed Key: %c|%d\n\n", dir, n, horP, verP, food, xf, yf, fakefood, xff, yff, key, key);
-			
-			for(i = 0; i < ARENA_WIDTH * ARENA_HEIGHT; i++)
-			{
-				fprintf(debug, "[x%d]:	%d	-|-	[y%d]:	%d\n", i, x[i], i, y[i]);
-			}
-			
-			fclose(debug);
-		}
-	
-	system("cls");
-	gotoxy(0, 0);
-	std:: cout << "Whoops, that was't supposed to happen. Please send the Debug.txt to the developer";
-
 	return EXIT_FAILURE;
 }
