08d2e03bb706a6269c801494398cff80359f1802
Saipher604 <ljsaipher013@googlemail.com>
2015-03-19 00:26:22 +0100

    big Update
    - encryption of highscores now working, though still very simple
    algorythm
    - changed the usage of the position arrays to a much more efficient and
    less stupid way. Arrays are now smaller too
    - updated code commentary
    - added generation of a debug file if somehow the game breaks out of the
    game loop
    - removed the old intro
    - removed unused variables
    - added key press X to let the game run at unlocked speed
    - fixed a little graphics bug when drawing the arena walls

--- Snake/Videospieltest_private.h
+++ Snake/Videospieltest_private.h
@@ -5,19 +5,19 @@
 #define VIDEOSPIELTEST_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.6.3.499"
+#define VER_STRING	"1.7.5.737"
 #define VER_MAJOR	1
-#define VER_MINOR	6
-#define VER_RELEASE	3
-#define VER_BUILD	499
+#define VER_MINOR	7
+#define VER_RELEASE	5
+#define VER_BUILD	737
 #define COMPANY_NAME	"MauProductions"
-#define FILE_VERSION	"1.6.3.499"
+#define FILE_VERSION	"1.7.5.737"
 #define FILE_DESCRIPTION	"It's a Snake"
 #define INTERNAL_NAME	"project K‰sesoﬂe"
 #define LEGAL_COPYRIGHT	"(C) Mau"
 #define LEGAL_TRADEMARKS	"MauTrademarks"
 #define ORIGINAL_FILENAME	"Snake"
 #define PRODUCT_NAME	"SNAAAAAAKEEE!"
-#define PRODUCT_VERSION	"1.6.3.499"
+#define PRODUCT_VERSION	"1.7.5.737"
 
 #endif /*VIDEOSPIELTEST_PRIVATE_H*/
